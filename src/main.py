a41e8ad4277a656d4de68468aae516c73f2052c1
TeckWIT <tant1@wit.edu>
2025-07-08 13:18:54 -0400

    new todo and testing out numpy

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 print("hello world")
+
+x = np.random.randn(50)
+y = np.random.randn(50)
+
+plt.figure(figsize=(8, 6))
+plt.scatter(x, y, alpha=0.6)
+plt.xlabel('X values')
+plt.ylabel('Y values')
+plt.title('Scatter Plot')
+plt.show()
